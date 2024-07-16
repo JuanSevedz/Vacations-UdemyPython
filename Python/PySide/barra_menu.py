8154adb59237d3fce2dfa7af62f2abc7e0a15246
JuanSevedz <sebitas.ved@gmail.com>
2024-07-16 11:51:06 -0500

    Menu-PySide

--- Python/PySide/barra_menu.py
+++ Python/PySide/barra_menu.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt, QSize
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtGui import QAction, QIcon, QKeySequence
 from PySide6.QtWidgets import QMainWindow, QApplication, QLabel, QToolBar, QStatusBar, QCheckBox
 
 
@@ -52,12 +52,44 @@ class VentanaPrincipal(QMainWindow):
         barra.addWidget(QCheckBox())
         barra.addWidget(QLabel('Salir'))
 
+        menu = self.menuBar()
+        menu_archivo = menu.addMenu('&Archivo')
+        menu_archivo.addAction(boton_nuevo)
+
+        # Agregamos una segunda opcion
+        menu_archivo.addAction(boton_guardar)
+
+        # Agregamos un separador
+        menu_archivo.addSeparator()
+
+        # Agregamos una tercera opcion
+        boton_salir = QAction('Salir', self)
+        menu_archivo.addAction(boton_salir)
+
+        # Submenu ayuda
+        boton_acerca_de = QAction(QIcon('acerca.png'), 'Acerca De', self)
+        menu_ayuda = menu.addMenu('Ayuda')
+        menu_ayuda.addAction(boton_acerca_de)
+
+        boton_acerca_de.triggered.connect(self.click_boton_acerca_de)
+
+        # Agregamos un submenu
+        menu_archivo.addMenu(menu_ayuda)
+
+        # Atajos
+        # boton_nuevo.setShortcut(QKeySequence('Ctrl+n'))
+        boton_nuevo.setShortcut(Qt.CTRL | Qt.Key_N)
+        boton_acerca_de.setShortcut(Qt.CTRL | Qt.Key_1)
+        boton_guardar.setShortcut(Qt.CTRL | Qt.Key_G)
+
+    def click_boton_acerca_de(self, s):
+        print(f'Acerca de... {s}')
 
     def click_boton_guardar(self, s):
         print(f'Guardando archivo... {s}')
 
     def click_boton_nuevo(self, s):
-        print(f'Nuevo archivo {s}')
+        print(f'Nuevo archivo... {s}')
 
 
 if __name__ == '__main__':
