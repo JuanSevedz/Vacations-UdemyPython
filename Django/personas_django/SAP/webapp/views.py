3314854910895a6cec00ed095f242a524bec32d8
JuanSevedz <sebitas.ved@gmail.com>
2024-07-24 08:47:00 -0500

    Validation-Django

--- Django/personas_django/SAP/webapp/views.py
+++ Django/personas_django/SAP/webapp/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from personas.models import Persona# se importa para poder interactuar con otro archivo de Django
+
 
 # Create your views here.
 def bienvenido(request):
-    return HttpResponse('Hola Mundo desde Django')
+    no_personas = Persona.objects.count()
+    personas = Persona.objects.all()
+    return render(request, 'bienvenido.html', {'no_personas' : no_personas, 'personas': personas})
 
-def despedirse(request):
-    return HttpResponse('Despidiendome desde Django')
