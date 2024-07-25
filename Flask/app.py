12f889d5078d5b4e00232e9a83843d664beb1ff2
JuanSevedz <sebitas.ved@gmail.com>
2024-07-25 13:39:14 -0500

    LoginLogout-Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,22 +1,68 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, jsonify, session
+from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
+app.secret_key = 'MySecretKey'
+
 
 @app.route('/')
-def hello_world():
-    app.logger.info(f'Entramos al path: {request.path}')# Para mandar infromacion a la consola
-    return 'Hello, World from world!.'
+def inicio():
+    if 'username' in session:
+        return f'El Usuario: {session["username"]}, ya ha hecho Login '
+    return 'NO a hecho Login'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        usuario = request.form['username']
+        # agregar el usuario a la sesion
+        session['username'] = usuario
+        return redirect(url_for('inicio'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('inicio'))
+
 
 @app.route('/saludar/<nombre>')
 def saludar(nombre):
     app.logger.info(f'Entramos al path: {request.path}')
     return f'Holiiiii {nombre}'
 
+
 @app.route('/edad/<int:edad>')
 def show_age(edad):
     return f'Tu edad es: {edad}'
 
+
 @app.route('/mostrar/<nombre>', methods=['GET', 'POST'])
 def mostrar_nombre(nombre):
     return render_template('mostrar.html', nombre=nombre)
+
+
+@app.route('/redireccionar')
+def redireccionar():
+    return redirect(url_for('inicio'))
+
+
+@app.route('/salir')
+def salir():
+    return abort(404)
+
+
+@app.errorhandler(404)
+def pagina_no_encontrada(error):
+    return render_template('error404.html', error=error), 404
+
+
+# REST -> Representational State Transfer
+@app.route('/api/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_json(nombre):
+    valores = {'nombre': nombre, 'metodo_http': request.method}
+    return jsonify(valores)
