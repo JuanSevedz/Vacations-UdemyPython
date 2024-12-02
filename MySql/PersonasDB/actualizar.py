e7325ab892441f57068d0506ff3ed0acad960c79
JuanSevedz <sebitas.ved@gmail.com>
2024-12-02 18:55:49 -0500

    Reloading

--- MySql/PersonasDB/actualizar.py
+++ MySql/PersonasDB/actualizar.py
@@ -1,8 +1,22 @@
-import mysql.connector  # type: ignore
+import os
+from dotenv import load_dotenv
+import mysql.connector
+
+load_dotenv()
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
 
 personas_db = mysql.connector.connect(
-    host="localhost", user="root", password="Sarita2023", database="personas_db"
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
 )
+
+print("Conexión exitosa a la base de datos.")
+
 
 cursor = personas_db.cursor()
 sentencia_sql = (
--- MySql/PersonasDB/seleccion.py
+++ MySql/PersonasDB/seleccion.py
@@ -1,11 +1,24 @@
-import mysql.connector # type: ignore
+import os
+from dotenv import load_dotenv
+import mysql.connector
 
+
+load_dotenv()
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+# Establecer la conexión a la base de datos
 personas_db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Sarita2023",
-    database="personas_db"
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
 )
+
+print("Conexión exitosa a la base de datos.")
+
 
 mi_cursor = personas_db.cursor()
 mi_cursor.execute('SELECT * FROM personas')
